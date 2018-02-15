ee36da13c1d8e3103c45d91bd0e5b9847d7ba08b
baba <bietz1@gmx.de>
2018-02-15 18:27:33 +0100

    final push

--- opencvimpro.py
+++ opencvimpro.py
@@ -1,15 +1,20 @@
 from PIL import Image
-import glob
+import argparse
 import cv2
+import glob
 import numpy as np
 import os
 
-imgpath = "AndroidStudio/android-OpenCV/app/src/main/res/drawable-xxhdpi/scene_3.jpg"
+if __name__ == '__main__':
+    # Let's allow the user to pass the filename as an argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image", default="test.jpg", type=str,
+                        help="Path to image")
+    args = parser.parse_args()
+    imgpath = args.image
 img = cv2.imread(imgpath, 1)
 
-imgname = os.path.splitext(imgpath)[0]
-imgextension = os.path.splitext(imgpath)[1]
-
+imagename, imageextension = os.path.splitext(os.path.basename(imgpath))
 
 hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 lower_red = cv2.inRange(hsv_image, (0, 100, 100), (10, 255, 255))
@@ -19,19 +24,15 @@ red_combined = cv2.GaussianBlur(red_combined, (9, 9), 2, 2)
 
 cv2.waitKey(0)
 circles = cv2.HoughCircles(red_combined, cv2.HOUGH_GRADIENT, 1, (red_combined.shape[0]) / 2,
-                           param1=100, param2=20, minRadius=10, maxRadius=0)
+                           param1=100, param2=20, minRadius=15, maxRadius=0)
 if circles is not None:
     circles = np.uint16(np.around(circles))
     c = 0
     height, width, chan = img.shape
+
     for i in circles[0, :]:
-        # draw the outer circle
-        # cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
-        # draw the center of the circle
-        # cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
         if i is None or i[2] > width/2:
             continue
-        #cropped = np.empty(i[2]*i[2], dtype='int64')
 
 
         print("x: " + str(i[0]) + "   y: " + str(i[1]) + "   rad: " + str(i[2]) + "  width: " + str(width) + " height: " + str(height))
@@ -43,15 +44,16 @@ if circles is not None:
         cropped = img[x1: x2, y1: y2]
         img2020 = cv2.resize(cropped, (20, 20))
         hsv_chans = cv2.split(img2020)
-        # retval, binary_image = cv2.threshold(hsv_chans[2], 150, 255, cv2.THRESH_BINARY)
 
         cv2.rectangle(img, (i[0] - i[2], i[1] - i[2]), (i[0] + i[2], i[1] + i[2]), (0, 255, 0), 2, 8, 0)
 
         retval, binary_image = cv2.threshold(hsv_chans[2], 150, 255, cv2.THRESH_BINARY)
 
+        croppedpath = imagename + "(" + str(c) + ")" + imageextension
+        cv2.imwrite(croppedpath, binary_image)
         cv2.imshow("Binary", binary_image)
         img2020 = cv2.resize(binary_image, (20, 20))
-        cv2.imwrite("10bin.png", binary_image)
+        cv2.imwrite(croppedpath, binary_image)
         c += 1
         cv2.waitKey(0)
 
